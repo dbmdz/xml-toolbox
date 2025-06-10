2a63a8361a4515914db3728d2acb1a165382e4d3
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2025-06-10 15:46:49 +0200

    Initial checkin; migrated from digitalcollections-commons, module dc-commons-xml

---
+++ src/main/java/dev/mdz/xml/xpath/XPathMappingException.java
@@ -0,0 +1,11 @@
+package dev.mdz.xml.xpath;
+
+public class XPathMappingException extends Exception {
+  public XPathMappingException(String s) {
+    super(s);
+  }
+
+  public XPathMappingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
